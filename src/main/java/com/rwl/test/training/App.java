19d9aa19fe8b6d9d2ca4ef0b160043705705614c
Thanthathon Buagird <thanthathon@geniustree.co.th>
2015-06-05 11:49:15 +0700

    add Unit Test for class Fibonacci

--- src/main/java/com/rwl/test/training/App.java
+++ src/main/java/com/rwl/test/training/App.java
@@ -23,12 +23,12 @@ public class App {
         int n;
 
         System.out.println("Fibonacci Algorithm\n");
-        System.out.print("Please input a number : ");
+        System.out.print("Please input a number of fibonacci seq : ");
         n = scanner.nextInt();
-        fibo = new int[n];
-        fibonacci(n);
+
+        fibo = Fibonacci.getFibonacciSequence(n);
+
         int length = fibo.length;
-        System.out.println("number of fibonacci is " + n);
         System.out.print("fibonacci sequence is ");
         for (int i = 0; i < length; i++) {
             if (i != length - 1) {
@@ -36,18 +36,6 @@ public class App {
             } else {
                 System.out.println(fibo[i]);
             }
-        }
-    }
-
-    private static int fibonacci(int n) {
-        if (n > 2) {
-            fibo[n - 1] = fibonacci(n - 1) + fibonacci(n - 2);
-            return fibo[n - 1];
-        } else if (n > 0) {
-            fibo[n - 1] = 1;
-            return 1;
-        } else {
-            throw new IllegalArgumentException("Number must be more than zero");
         }
     }
 }
